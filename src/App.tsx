50b3681e75f4df78d8f79b550f0d9e82ad43126e
tamjidzihan <tamjidzihan@gmail.com>
2024-06-10 17:15:40 +0600

    install vite, mantain UI, react-icon

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import { AppShell, Burger } from '@mantine/core'
+import './App.css'
+import { useDisclosure } from '@mantine/hooks';
+
+function App() {
+
+
+  return (
+
+    <>
+      <AppShell>
+        <AppShell.Navbar>
+
+        </AppShell.Navbar>
+      </AppShell>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import {
+    GrHomeRounded,
+    GrUser,
+    GrTerminal,
+    GrDocumentText,
+    GrTask
+} from "react-icons/gr";
+import { FiGithub } from "react-icons/fi";
+import logo from '../../assets/logo_dev.png'
+
+
+import React from 'react'
+
+
+
+const mainLinksMockdata = [
+    { icon: GrHomeRounded, label: 'GrHomeRounded' },
+    { icon: GrUser, label: 'About' },
+    { icon: GrTerminal, label: 'Projects' },
+    { icon: GrDocumentText, label: 'Resume' },
+    { icon: GrTask, label: 'Blog' },
+    { icon: FiGithub, label: 'Git' },
+];
+
+function Navbar() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Navbar
