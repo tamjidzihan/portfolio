46e7698012e93ea6656184e0452eeae5afd362e6
tamjidzihan <tamjidzihan@gmail.com>
2024-09-30 18:37:03 +0600

    office 2024-09-30 resume page finalize

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 // import Particle from "../Particle";
-import { Container, Row } from "react-bootstrap";
+import { Container, Image, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import pdf from "../../Assets/../Assets/Tamzid_zihan.pdf"
+import CV from "../../Assets/ZIHAN_CV.jpg"
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.12.313/pdf.worker.min.js`;
 
@@ -15,8 +16,6 @@ function ResumeNew() {
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
-
-  const ver = pdfjs.version
 
   return (
     <div>
@@ -35,9 +34,10 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          {/* <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          </Document> */}
+          <Image src={CV} fluid className="w-50" onContextMenu={(e) => e.preventDefault()} />
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
